44e99c78628d8c71546f255437cb12d599b0549b
Fredrik Ekelund <fredrik@fredrik.computer>
2016-05-26 16:00:50 +0200

    Updated Puppet manifests and setup files
    Now we get Grav instead of Wordpress!

---
+++ puppet/modules/grav/manifests/init.pp
@@ -0,0 +1,23 @@
+# Install latest Grav
+
+class grav::install {
+
+  exec { 'download-grav':
+    command => '/usr/bin/wget https://github.com/getgrav/grav/releases/download/1.0.10/grav-admin-v1.0.10.zip',
+    cwd     => '/vagrant/',
+    creates => '/vagrant/grav-admin-v1.0.10.zip'
+  }
+
+  package{ 'unzip':
+    ensure => present,
+    notify => Exec['unzip-grav']
+  }
+
+  exec { 'unzip-grav':
+    cwd     => '/vagrant/',
+    command => '/usr/bin/unzip /vagrant/grav-admin-v1.0.10.zip',
+    require => Exec['download-grav'],
+    creates => '/vagrant/grav-admin',
+  }
+
+}
